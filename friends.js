5574ab06ce54f595a8975662c3b4f0c2ce3902ac
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-07-18 17:51:46 -0700

    Add travel array

--- friends.js
+++ friends.js
@@ -3,3 +3,9 @@ var arrays = ["Jenny", "Heather", "Erika", "Beth", "Susan"];
 console.log(arrays[3]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var travel = ["Montana", "Washington", "Texas", "Arizona", "Colorado"];
+
+travel.pop();
+
+console.log(travel);
